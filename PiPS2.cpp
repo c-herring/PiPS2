5e5549734855f66b47015395a7764afb47f23a62
c-herring <chrisherring1988@gmail.com>
2015-02-28 23:36:53 +0800

    First PiPS2 code commit

---
+++ PiPS2.cpp
@@ -0,0 +1,226 @@
+#include "PiPS2.h"
+#include <wiringPi.h>
+#include <stdio.h>
+
+// Initialize the I/O pins and set up the controller for the desired mode.
+//	TODO:
+//		This function is hard coded to configure controller for analog full mode.
+//		Must also implement input parameters to choose what mode to use.
+//	Inputs:
+// 			--!! NOTE !!-- wiringPiSetupPhys(), wiringPiSetupGpio() OR wiringPiSetup()
+// 				should be called first. The following pins refer to either the gpio or the 
+//				physical pins, depending on how wiring pi was set up.
+// 		_commandPin - The RPi pin that is connected to the COMMAND line of PS2 remote 
+//  	_dataPin	- The RPi pin that is connected to the DATA line of PS2 remote
+//  	_clkPin		- The RPi pin that is connected to the CLOCK line of PS2 remote
+// 		_attnPin	- The RPi pin that is connected to the ATTENTION line of PS2 remote
+//	Returns:
+//		0 -		Config success.
+//		1 - 	Controller is in unknown mode or no controller found.
+//		2 - 	Controller can be read but will not accept commands.
+unsigned char PIPS2::initializeController(int _commandPin, int _dataPin, int _clkPin, int _attnPin)
+{
+	
+	// INITIALIZE I/O
+	commandPin = _commandPin;
+	dataPin = _dataPin;
+	clkPin = _clkPin;
+	attnPin = _attnPin;
+	readDelay = 1;
+	// Set command pin to output
+	pinMode(commandPin, OUTPUT);
+	// Set data pin to input
+	pinMode(dataPin, INPUT);
+	// Set attention pin to output
+	pinMode(attnPin, OUTPUT);
+	// Set clock pin to output
+	pinMode(clkPin, OUTPUT);
+	
+	// Set command pin and clock pin high, ready to initialize a transfer.
+	digitalWrite(commandPin, 1);
+	digitalWrite(clkPin, 1);
+
+	// Read controller a few times to check if it is talking.
+	readPS2();	
+	readPS2();
+	// See if controller responded.
+	if(PS2data[1] != DIGITALMODE && PS2data[1] != ANALOGMODE && PS2data[1] != DS2NATIVEMODE)
+	{
+		// Mode is unknown or controller unresponsive.
+		return 1;
+	}
+	
+	// Initialize the read delay to be 1 millisecond. 
+	// Increment read_delay until controller accepts commands.
+	// This is a but of dynamic debugging. Read delay usually needs to be about 2.
+	// But for some controllers, especially wireless ones it needs to be a bit higher.
+	
+	// Try up until readDelay = 10
+	for (int i = 0; i <= 10; i++)
+	{
+		// Transmit the enter config command.
+		transmitCmdString(enterConfigMode, sizeof(enterConfigMode));
+		
+		// Set mode to analog mode and lock it there.
+		transmitCmdString(set_mode_analog_lock, sizeof(set_mode_analog_lock));
+		
+		// Return all pressures
+		//transmitCmdString(config_AllPressure, sizeof(config_AllPressure));
+		// Exit config mode.
+
+		
+		transmitCmdString(exitConfigMode, sizeof(exitConfigMode));
+		
+		// Attempt to read the controller.
+		readPS2();
+		
+		// If read was successful (contorller indicates it is in analog mode), break this config loop.
+		if(PS2data[1] == ANALOGMODE)
+		break;
+	
+		// If we have tried and failed 10 times. call it quits,
+		if (i == 10)
+		{
+			return 2;
+		}
+		
+		// Otherwise increment the read delay and go for another loop
+		readDelay += 1;	
+	}
+	return 0;
+}
+
+// Bit bang a single byte.
+// Inputs:
+// 		byte 	- The byte to transmit.
+//
+// Returns:
+// 		
+unsigned char PIPS2::transmitByte(char byte)
+{
+	// Data byte received
+	unsigned char RXdata = 0;
+	
+	// Bit bang all 8 bits
+	for (int i = 0; i < 8; i++)
+	{
+		// If the bit to be transmitted is 1 then set the command pin to 1
+		if (CHK(byte, i)) 	digitalWrite(commandPin, 1);
+		// Otherwise set it to 0.
+		else				digitalWrite(commandPin, 0);
+		
+		// Pull clock low to transfer bit
+		digitalWrite(clkPin, 0);
+		
+		// Wait for the clock delay before reading the received bit.
+		delayMicroseconds(CLK_DELAY);
+		
+		// If the data pin is now high then save the input.
+		if (digitalRead(dataPin)) SET(RXdata, i);
+		
+		// Done transferring bit. Put clock back high
+		digitalWrite(clkPin, 1);
+		delayMicroseconds(CLK_DELAY);
+	}
+	
+	// Done transferring byte, set the command pin back high and wait.
+	digitalWrite(commandPin, 1);
+	delayMicroseconds(BYTE_DELAY);
+	return RXdata;
+}
+
+// Bit bang out an entire string of specified length.
+//
+// Inputs:
+// 		string 	- Pointer to unsigned char array to be transmitted.
+// 		len 	- Number of bytes to be transmitted.
+void PIPS2::transmitCmdString(unsigned char *string, int len)
+{
+	// Create a temporary buffer for receiving the response.
+	unsigned char tempBuffer[len];
+	
+	// Ready to begin transmitting, pull attention low.
+	digitalWrite(attnPin, 0);
+	
+	// Shift the data out, one byte at a time.
+	for (int y = 0; y < len; y++)
+	{
+		tempBuffer[y] = transmitByte(string[y]);
+	}
+	
+	// Packet finished, release attention line.
+	digitalWrite(attnPin, 1);
+	// Wait some time before beginning another packet.
+	delay(readDelay);
+	
+}
+
+// Read the PS2 Controller. Save the returned data to PS2data.
+void PIPS2::readPS2(void)
+{
+	static unsigned int last_read = 0;
+	unsigned int timeSince = millis() - last_read;
+	if (timeSince > 1500) //waited to long
+		reInitializeController();
+	if(timeSince < readDelay)  //waited too short
+		delay(readDelay - timeSince);	
+	// Ensure that the command bit is high before lowering attention.
+	digitalWrite(commandPin, 1);
+	// Ensure that the clock is high.
+	digitalWrite(clkPin, 1);
+	// Drop attention pin.
+	digitalWrite(attnPin, 0);
+	
+	// Wait for a while between transmitting bytes so that pins can stabilize.
+	delayMicroseconds(BYTE_DELAY);
+	
+	// The TX and RX buffer used to read the controller.
+	unsigned char TxRx1[9] = {0x01,0x42,0,0,0,0,0,0,0};
+	unsigned char TxRx2[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
+	
+	// Grab the first 9 bits
+	for (int i = 0; i<9; i++)
+	{
+		PS2data[i] = transmitByte(TxRx1[i]);
+	}
+	
+	// If controller is in full data return mode, get the rest of data
+	if(PS2data[1] == 0x79)
+	{  
+		for (int i = 0; i<12; i++)
+		{
+			PS2data[i+9] = transmitByte(TxRx2[i]);
+			//printf("%d: %d\t->\t%d\n", i, (int)TxRx1[i], (int)PS2data[i]);
+		}
+	}
+	
+	// Done reading packet, release attention line.
+	digitalWrite(attnPin, 1);
+	last_read = millis();
+	   
+}
+
+// Re-Initialize the I/O pins and set up the controller for the desired mode.
+//	TODO:
+//		This function is hard coded to configure controller for analog full mode.
+//		Must also implement input parameters to choose what mode to use.
+//	Inputs:
+// 			--!! NOTE !!-- wiringPiSetupPhys(), wiringPiSetupGpio() OR wiringPiSetup()
+// 				should be called first. The following pins refer to either the gpio or the 
+//				physical pins, depending on how wiring pi was set up.
+// 		_commandPin - The RPi pin that is connected to the COMMAND line of PS2 remote 
+//  	_dataPin	- The RPi pin that is connected to the DATA line of PS2 remote
+//  	_clkPin		- The RPi pin that is connected to the CLOCK line of PS2 remote
+// 		_attnPin	- The RPi pin that is connected to the ATTENTION line of PS2 remote
+unsigned char PIPS2::reInitializeController(void)
+{
+	transmitCmdString(enterConfigMode, sizeof(enterConfigMode));
+	transmitCmdString(set_mode_analog_lock, sizeof(set_mode_analog_lock));
+	transmitCmdString(config_AllPressure, sizeof(config_AllPressure));
+	transmitCmdString(exitConfigMode, sizeof(exitConfigMode));
+}
+
+
+
+
+
---
+++ PiPS2.h
@@ -0,0 +1,74 @@
+/* Bit Operation Macros */
+#define SET(x,y) (x|=(1<<y))	// Set bit y in x
+#define CLR(x,y) (x&=(~(1<<y))) // Clear bit y in x
+#define CHK(x,y) (x & (1<<y)) 	// Check if bit y in x is set
+#define TOG(x,y) (x^=(1<<y)) 	// Toggle bit y in x
+
+/* Delays */
+#define CLK_DELAY  4
+#define BYTE_DELAY 3
+
+/* Controller Modes - From: http://www.lynxmotion.com/images/files/ps2cmd01.txt */
+#define DIGITALMODE 	0x41
+#define ANALOGMODE 		0x73
+#define DS2NATIVEMODE	0xF3
+
+/* Button Masks */
+// 		From data bit 0 (PS2data[3])
+#define BTN_SELECT 		0
+#define	BTN_LEFT_JOY	1
+#define	BTN_RIGHT_JOY	2
+#define	BTN_START		3
+#define	BTN_UP			4
+#define	BTN_RIGHT		5
+#define	BTN_DOWN		6
+#define	BTN_LEFT		7
+// 		From data bit 1 (PSdata[4])
+#define	BTN_L2			0
+#define	BTN_R2			1
+#define	BTN_L1			2
+#define	BTN_R1			3
+#define	BTN_TRIANGLE	4
+#define	BTN_CIRCLE		5
+#define	BTN_X			6
+#define	BTN_SQUARE		7
+
+/* Byte Numbers of PSdata[] For Button Pressures */
+#define	PRES_RIGHT 		10
+#define	PRES_LEFT 		11
+#define	PRES_UP 		12
+#define	PRES_DOWN 		13
+#define	PRES_TRIANGLE 	14
+#define	PRES_CIRCLE 	15
+#define	PRES_X		 	16
+#define	PRES_SQUARE 	17
+#define	PRES_L1		 	18
+#define	PRES_R1		 	19
+#define	PRES_L2		 	20
+#define	PRES_R2		 	21
+
+/* Controller Commands */
+static unsigned char 	enterConfigMode[]={0x01,0x43,0x00,0x01,0x00};
+static unsigned char 	set_mode_analog_lock[]={0x01,0x44,0x00,0x01,0x03,0x00,0x00,0x00,0x00};
+static unsigned char 	exitConfigMode[]={0x01,0x43,0x00,0x00,0x5A,0x5A,0x5A,0x5A,0x5A};
+static unsigned char 	type_read[]={0x01,0x45,0x00,0x5A,0x5A,0x5A,0x5A,0x5A,0x5A};
+static unsigned char 	config_AllPressure[]={0x01,0x4F,0x00,0xFF,0xFF,0x03,0x00,0x00,0x00};
+
+/* Class declaration */
+class PIPS2 {
+	public:
+		unsigned char initializeController(int _commandPin, int _dataPin, int _clkPin, int _attnPin);
+		unsigned char reInitializeController(void);
+		unsigned char transmitByte(char byte);
+		void transmitCmdString(unsigned char *string, int length);
+		void readPS2(void);
+		unsigned char PS2data[21];
+		
+	private:
+		int commandPin;
+		int dataPin;
+		int attnPin;
+		int clkPin;
+		unsigned int readDelay;
+		
+};
